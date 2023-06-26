89d86005c0f34ef0790b0017dd8bf628e7de9e60
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2023-06-26 12:08:29 +0300

    refactor: ReactDOM v18 added
    For now, we use the index.tsx as an entry point for our local dev server.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-// In ContextProviders.tsx
 import DrawerProvider from '@/context/Drawer'
-import { ReactNode } from 'react'
-// import { ThemeProvider } from './ThemeContext';
-// import { UserProvider } from './UserContext';
-// import { LocaleProvider } from './LocaleContext';
-// import other providers...
+import React, { ReactNode } from 'react'
+import { createRoot } from 'react-dom/client'
 
 export const ContextProviders = ({ children }: { children: ReactNode }) => {
   return <DrawerProvider>{children}</DrawerProvider>
 }
 
-const element = document.getElementById('root')
+const element = document.getElementById('container')
 
-const root = createRoot(document.getElementById('container'))
+if (!element) {
+  throw new Error("Couldn't find root element")
+}
+
+const root = createRoot(element)
+
 root.render(
   <React.StrictMode>
     <ContextProviders>
