3f8697a9f6f5eab193c27763ae78d7841e334662
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2023-06-28 10:02:43 +0300

    feat: configuration
    Added configuration file that exports a default configuration for the AppShell.

---
+++ assets/config.ts
@@ -0,0 +1,14 @@
+export default {
+  drawer: {
+    anchor: 'right',
+    width: 70,
+    backdropClickClose: true,
+    content: null,
+    open: false,
+    PaperProps: {},
+    ModalProps: {},
+    sx: {},
+    fabProps: {},
+    closeIconProps: {},
+  },
+}
