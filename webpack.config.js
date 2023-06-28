78e7678339c98c25276c0da045f3addf7fa0d3d5
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2023-06-28 10:01:41 +0300

    ref: relocation index.html
    deleted the 'public' folder, moved the index.html template to the 'dev' folder.

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = (env, options) => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html'),
+        template: path.resolve(__dirname, 'dev', 'index.html'),
       }),
     ],
     devServer: {
